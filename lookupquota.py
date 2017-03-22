e276eb7123c349eda1545735cc5920e37bc7b99a
SimonH <simonslhui@gmail.com>
2017-03-22 01:00:50 +0000

    Draft 22/03

---
+++ lookupquota.py
@@ -0,0 +1,11 @@
+"""
+>>>lookupquota({"Jack":"Simon, Ivan, Rex"},4)
+Tutees assigned to Jack: 3
+Available quotas: 1
+"""
+#C1670764
+from collections import Counter
+
+def lookupquota(dictionary, quota):
+	print("Add something here.")
+	pass
