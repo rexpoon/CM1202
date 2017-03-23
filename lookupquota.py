4bb462a080dc00061f79738ff7b85a67db7c09ba
SimonH <simonslhui@gmail.com>
2017-03-23 19:08:43 +0000

    Renamed again

--- lookupquota.py
+++ lookupquota.py
@@ -1,18 +1,13 @@
-"""
->>>lookupquota(assignment,4)
-Tutees assigned to Jack: 3
-Available quotas for Jack: 1
-Tutees assigned to Matt: 4
-Available quotas for Matt: 0
-"""
 #C1670764
+
 from collections import Counter
 import system
 
-def lookupquota(dictionary,quota):
+
+def lookupquota(dictionary):
 	for item in dictionary:
 		assigned= len(dictionary[item])
 		print("Tutees assigned to "+item+": "+str(assigned))
-		available= (quota-assigned)
+		available= (UGquota-assigned)
 		print("Available quotas for "+item+": "+str(available))
 	pass
