f3a737d1167ab649c43d08757e597ddc711fc54a
SimonH <simonslhui@gmail.com>
2017-03-23 18:58:28 +0000

    Filled the details in assignment

---
+++ assignment.py
@@ -0,0 +1,17 @@
+# All Python functions should use this assignment dictionary!
+# Use import * from assignment at the front of your Python file
+
+#Current assignment - format:{tutorname:[studentname1,studentname2]}
+assignment = {"Alia I Abdelmoty":["Zachariah Butler","Joshua Davies","Samuel Healey"],"Martin Caminada":["Ahamed Abdul Kayee","Alex Cheung","Samuel East"],"Matthew John":["Charlie Bennett","Kieran Fewell","Robin Watson","Joshua Wong"],"Yukun Lai":["Mia Davis","Lloyd Francis","Nicole Kan"], "Kirill Sidorov":["Zachary Allen","Giorgos Andreou","Max Milburn"]}
+
+#tutor list and its assigned tutee class, 0 for UnderGrad, 1 for PostGrad - format:{tutorname:tuteeclass}
+tutorlist = {"Alia I Abdelmoty":0,"Martin Caminada":1,"Matthew John":0,"Yukun Lai":0, "Kirill Sidorov":1}
+
+#the quota of the number of tutees a tutor handles
+UGquota = 5
+PGquota = 3
+
+
+
+
+
