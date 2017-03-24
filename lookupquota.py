99085fc1ba3da7c9aa8f7382036381b08f3c57e7
SimonH <simonslhui@gmail.com>
2017-03-24 01:00:09 +0000

    Put the GUI on the lookupquota

--- lookupquota.py
+++ lookupquota.py
@@ -6,12 +6,14 @@ from tkinter import *
 from tkinter.messagebox import *
 
 app = Tk()
+box = Listbox(app, width=75)
+box.insert(0,"Tutor Name, Assigned tutees, Assigned quota, Available quota")
 
 def lookupquota():
 	dictionary = assignment
+	i = 1
 	for item in dictionary:
 		assigned= len(dictionary[item])
-		print("Tutees assigned to "+item+": "+str(assigned))
 		for key in tutorlist:
 			if item == key:
 				tuteeclass = tutorlist[key]
@@ -21,18 +23,18 @@ def lookupquota():
 			quota = PGquota
 		else:
 			raise Exception("The quota of a tutor has not been specified. Please check the userdata.")
-
 		available= (quota-assigned)
-		print(item+" has a quota of "+str(quota)+" tutees.")
-		print("Available quotas for "+item+": "+str(available))
-	pass
+		exportlist=[item, assigned, quota, available]
+		box.insert(i,exportlist)
+		i += 1
 
+app.title('Quotas of tutees of every staff member')
 app.geometry('500x500+450+140')
+app.configure(bg='cornflower blue')
+box.pack(command=lookupquota())
 
-Label(app, text='Tutor name').grid(row=0,column=1, sticky=W)
-Label(app, text='Number of tutees').grid(row=0, column=2, sticky=W)
-Label(app, text='Tutees quota').grid(row=0,column=3, sticky=W)
-Label(app, text='Available quota').grid(row=0,column=4, sticky=W)
+
+
 
 
 app.mainloop()
