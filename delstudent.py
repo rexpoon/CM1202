07643803aaaf2b1d58cc22840f99598f1ef80c83
ivanshum2009 <ivanshum2009@hotmail.com>
2017-03-24 02:46:43 +0000

    fk it

--- delstudent.py
+++ delstudent.py
@@ -20,7 +20,7 @@ def messagePop():
 	for key, value in assignment.items():
 		for v in value:
 			if name in v:
-				messagebox.showinfo("ddddd",v+" has been deleted from the group of tutor: "+key)
+				messagebox.showinfo("Delete Student",v+" has been deleted from the group of tutor: "+key)
 
 
 
@@ -34,24 +34,28 @@ COLORS  =['blue', 'gold', 'cornflower blue']
 app.geometry('500x500+450+140')
 
 # The title of the program
-app.title('Delete student')
+app.title('Delete a student from the tutor list')
 
 
 # Object positioning in the program
 Label(app, text="Please enter the first and last name of student.").place(x=10,y=0)
-Label(app, text= "Full Name:", bg="gold", fg="blue").place(x=10,y=120)
-
+Label(app, text="First Name:", bg="gold", fg="blue").place(x=10,y=120)
+Label(app, text="Last Name:", bg="gold", fg="blue").place(x=10,y=170)
+Label(app, text="alternative tutor:", bg="gold", fg="blue").place(x=10,y=220)
 
 # Entry
 def get_data():
     x_data = x.get()
-    print ("x_data = {0}".format(x_data))
+    y_data = y.get()
+    print ("x_data = {0} , y_data = {1} ".format(x_data,y_data))
 
 x = Entry(app)
-
+y = Entry(app)
 
 
 x.place(x=90,y=120)
+y.place(x=90,y=170)
+
 
 
 # Buttons
