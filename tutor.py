1aad650d78faed26e47907b8fee387315de514f5
rexpoon <rexpoon2020@hotmail.com>
2017-03-24 02:00:00 +0000

    Add files via upload
    I have completed this search tutor

--- tutor.py
+++ tutor.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter.messagebox import *
-#import userdata
 from userdata import *
 
 
@@ -11,39 +10,13 @@ def messagePop():
 
     get_data()
 
-    for item in assignment:
-        if item.find("Zachariah Butler"):
-            messagebox.showinfo("Result", "Alia I Abdelmoty")
-        elif item.find("Samuel East"):
-            messagebox.showinfo("Result", "Martin Caminada")
-        elif item.find("Robin Watson"):
-            messagebox.showinfo("Result", "Matthew John")
-        elif item.find("Joshua Davies"):
-            messagebox.showinfo("Result", "Alia I Abdelmoty")
-        elif item.find("Samuel Healey"):
-            messagebox.showinfo("Result", "Alia I Abdelmoty")
-        elif item.find("Ahamed Abdul Kayee"):
-            messagebox.showinfo("Result", "Martin Caminada")
-        elif item.find("Alex Cheung"):
-            messagebox.showinfo("Result", "Martin Caminada")
-        elif item.find("Charlie Bennett"):
-            messagebox.showinfo("Result", "Matthew John")
-        elif item.find("Kieran Fewell"):
-            messagebox.showinfo("Result", "Matthew John")
-        elif item.find("Joshua Wong"):
-            messagebox.showinfo("Result", "Matthew John")
-        elif item.find("Mia Davis"):
-            messagebox.showinfo("Result", "Yukun Lai")
-        elif item.find("Lloyd Francis"):
-            messagebox.showinfo("Result", "Yukun Lai")
-        elif item.find("Nicole Kan"):
-            messagebox.showinfo("Result", "Yukun Lai")
-        elif item.find("Zachary Allen"):
-            messagebox.showinfo("Result", "Kirill Sidorov")
-        elif item.find("Giorgos Andreou"):
-            messagebox.showinfo("Result", "Kirill Sidorov")
-        elif item.find("Max Milburn"):
-            messagebox.showinfo("Result", "Kirill Sidorov")
+    name = x.get() and y.get()
+
+    for key, value in assignment.items():
+        for v in value:
+            if name in v:
+                messagebox.showinfo("Result", v + " 's personal tutor is " +key)
+
 
 app.configure(bg='cornflower blue')
 
@@ -79,4 +52,3 @@ y.place(x=90,y=170)
 B1 = Button(app,text='Search',bg='gold',fg='blue', command = messagePop).place(x=425,y=450)
 
 app.mainloop()
-
